63a308d264e66ad8ae4ef36aa61712b8f12a9800
sabnit kaur <sabnitkaur01@sabnits-MacBook-Pro.local>
2021-03-12 23:38:09 -0500

    add css

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,14 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
 import TheMovieThumb from './components/TheMovieThumbnailComponent.js';
+import TheMusicThumb from './components/TheMusicThumbnailComponent.js';
+
 
 (() => {
     const vm = new Vue({
         data: {
-            allMovies: []
+            allMovies: [],
+            allMusic: []
+
         },
 
         created: function() {
@@ -14,6 +18,13 @@ import TheMovieThumb from './components/TheMovieThumbnailComponent.js';
                 console.table(data);
                 this.allMovies = data;
             })
+
+            fetch('/api/music')
+            .then(res => res.json())
+            .then(data => {
+                console.table(data);
+                this.allMusic = data;
+            })
             .catch(err => console.error(err));
         },
 
@@ -22,7 +33,9 @@ import TheMovieThumb from './components/TheMovieThumbnailComponent.js';
         },
 
         components: {
-            moviethumb: TheMovieThumb
+            moviethumb: TheMovieThumb,
+            musicthumb: TheMusicThumb
+
         }
 
     }) .$mount("#app");
